3e76b53686c7b3db09cf94ad4c93d49e28e012c6
Ike <i.nkisi-orji@rgu.ac.uk>
2020-09-03 08:45:35 +0100

    Update instruction for getting the vector representation of text

--- serverless-functions/config.sample.py
+++ serverless-functions/config.sample.py
@@ -7,4 +7,6 @@ aws = {
 }
 
 # Third-party APIs
+# use_vectoriser end-point should be callable using post request 
+# with body "{'text': text}" and should return a 512-dimensional vector representation of the text
 use_vectoriser = None # "https://link-to-endpoint-that-returns-vectors"
