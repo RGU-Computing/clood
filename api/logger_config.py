94311f0165f3e5749f7d8711b5c3c224c858bc10
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2024-06-12 15:11:55 +0100

    Added Logger System

---
+++ api/logger_config.py
@@ -0,0 +1,54 @@
+import logging
+import logging.config
+import os
+import colorlog
+
+def setup_logging():
+    log_dir = 'logs'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    logging_config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            },
+            'colored': {
+                '()': 'colorlog.ColoredFormatter',
+                'format': '%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                'log_colors': {
+                    'DEBUG': 'cyan,bg_white',
+                    'INFO': 'green',
+                    'WARNING': 'yellow',
+                    'ERROR': 'red',
+                    'CRITICAL': 'red,bg_white',
+                },
+            },
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'colored',
+            },
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'standard',
+                'filename': os.path.join(log_dir, 'application.log'),
+                'maxBytes': 1000 * 1024 * 1024,  # 1000 MB
+                'backupCount': 5,
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['console', 'file'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        }
+    }
+
+    logging.config.dictConfig(logging_config)
